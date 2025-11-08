02903bb795312bb1bfd9084dd7e66208fe831328
Layan-6 <11956@holbertonstudents.com>
2025-11-08 17:36:48 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,10 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Input/Output and Memory */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
+/* String Operations */
 #include <string.h>
+
+/* System Interface */
+#include <unistd.h>
+
+/* Process Management */
 #include <sys/wait.h>
 
 #endif /* MAIN_H */
