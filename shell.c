68d43a03cba912acebc2b5517305506a7aa9bc36
Afnan9999 <afnan-911@outlook.sa>
2025-11-09 18:40:49 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,83 +1,40 @@
-cat << 'EOF' > shell.c
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-extern char **environ;
 
 int main(void)
 {
-    char *line = NULL;
+    char *line = NULL, *cmd_path = NULL;
     size_t len = 0;
-    ssize_t nread;
     char *args[2];
-    pid_t pid;
-    char *cmd_path;
+    ssize_t read;
 
     while (1)
     {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "$ ", 2);
-
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            exit(0);
-        }
+        write(STDOUT_FILENO, "$ ", 2);
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+            break;
 
         line[strcspn(line, "\n")] = '\0';
 
-        if (strcmp(line, "env") == 0)
-        {
-            print_env();
-            continue;
-        }
-
         if (strcmp(line, "exit") == 0)
-        {
-            free(line);
-            exit(0);
-        }
+            break;
 
-        cmd_path = find_command_path(line);
-        if (!cmd_path)
-        {
-            perror("Command not found");
-            continue;
-        }
-
-        args[0] = cmd_path;
+        args[0] = line;
         args[1] = NULL;
 
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("fork");
-            free(cmd_path);
-            free(line);
-            exit(1);
-        }
-
-        if (pid == 0)
+        cmd_path = find_command_path(line);
+        if (cmd_path)
         {
             if (execve(cmd_path, args, environ) == -1)
-            {
                 perror("execve");
-                exit(127);
-            }
+            free(cmd_path);
         }
         else
         {
-            wait(NULL);
-            free(cmd_path);
+            write(STDOUT_FILENO, "Command not found: No such file or directory\n", 46);
         }
     }
 
     free(line);
     return (0);
 }
-EOF
