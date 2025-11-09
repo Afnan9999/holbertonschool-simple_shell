8f4e72459fa3b196b3b63ddff382b64ae2deb925
Afnan9999 <afnan-911@outlook.sa>
2025-11-09 19:03:28 +0300

    Create env.c

---
+++ env.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern char **environ;
+
+void print_env(void)
+{
+    int i = 0;
+    while (environ[i])
+    {
+        printf("%s\n", environ[i]);
+        i++;
+    }
+}
+EOF
